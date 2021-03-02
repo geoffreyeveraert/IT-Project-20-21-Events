e26c428c02a326a8854b5fadcb8120849090c457
JorisDenissen <joris.denissen@mail.be>
2021-03-02 20:34:52 +0100

    Eerste test fetch

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require("node-fetch");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res, next) => {
+  // Hier komt de fetch
+  let fetchResult;
+    fetch('https://app.ticketmaster.com/discovery/v2/events?apikey=lEQ7UsGACLAA2yaJ47Xt5hJPLK75W3Is&locale=*')
+    .then((response) => response.json)
+    .then((json) => console.log(json))
+    .catch((e) => console.error(e))
+
+  res.render('index', { result: fetchResult });
 });
 
 module.exports = router;
