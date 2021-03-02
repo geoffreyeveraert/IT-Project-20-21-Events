aa5828df5f0ebca19518b25408b1d6ab333f5c25
JorisDenissen <joris.denissen@mail.be>
2021-03-02 20:49:01 +0100

    Succesvolle fetch van de api

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,10 @@ router.get('/', async (req, res, next) => {
   // Hier komt de fetch
   let fetchResult;
     fetch('https://app.ticketmaster.com/discovery/v2/events?apikey=lEQ7UsGACLAA2yaJ47Xt5hJPLK75W3Is&locale=*')
-    .then((response) => response.json)
-    .then((json) => console.log(json))
+    .then((response) => response.json())
+    .then((json) => res.render('index', { result: JSON.stringify(json)}))
     .catch((e) => console.error(e))
 
-  res.render('index', { result: fetchResult });
 });
 
 module.exports = router;
