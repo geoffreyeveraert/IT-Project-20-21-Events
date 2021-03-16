f8a583647062bb0f4a78c73705ff4c19403da10f
Sciirof <geoff.everaert@gmail.com>
2021-03-16 18:57:42 +0100

    Changed list route back to app port and use bootstrap layout

--- routes/index.js
+++ routes/index.js
@@ -6,25 +6,25 @@ const app = express();
 const ejs = require('ejs');
 //let ejs = require("ejs");
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 
 const url = 'https://app.ticketmaster.com/discovery/v2/events?apikey=lEQ7UsGACLAA2yaJ47Xt5hJPLK75W3Is&locale=*';
 
-/*async function getapi(url) { 
-    
+async function getapi(url) {
+
   // Storing response 
-  const response = await fetch(url); 
-  
+  const response = await fetch(url);
+
   // Storing data in form of JSON 
-  var data = await response.json(); 
-  console.log(data); 
-} 
-getapi(url);*/
+  var data = await response.json();
+  console.log(data);
+}
+getapi(url);
 
 /* GET home page. */
-/*router.get("/", async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   // Hier komt de fetch
   fetch(url)
     .then((response) => response.json())
@@ -32,41 +32,33 @@ getapi(url);*/
       res.render("index", { result: JSON.stringify(json._embedded.events) })
     )
     .catch((e) => console.error(e));
-});*/
+});
 
-const loadAllEventData = async() => {
-     let response = await fetch('https://app.ticketmaster.com/discovery/v2/events?apikey=lEQ7UsGACLAA2yaJ47Xt5hJPLK75W3Is&locale=*');
-     let json = await response.json();
-     let event = json._embedded.events;
-     
-  
-  return event ;
-}
-
-const getMyEvents = (allEvents) =>{
-  let eventsName = [];
-  allEvents
-      .map(eventsNameMap=>
-          eventsName.push({name: eventsNameMap.name}
-      )
-  )
-  return eventsName;       
-}
-
-(async () => {
-
+router.get('/list', async (req, res, next) => {
   const allEvents = await loadAllEventData();
 
   const toonEvents = getMyEvents(allEvents)
   console.log(toonEvents);
-
-  app.listen(8000, () => {
-      console.log('Server started on port 8000');
-  });
-
-  app.get('/list',async(req,res) => {
-    res.render('list',{ allEvents: toonEvents });
+  res.render('list', { allEvents: toonEvents });
 });
-})();
+
+const loadAllEventData = async () => {
+  let response = await fetch('https://app.ticketmaster.com/discovery/v2/events?apikey=lEQ7UsGACLAA2yaJ47Xt5hJPLK75W3Is&locale=*');
+  let json = await response.json();
+  let event = json._embedded.events;
+
+
+  return event;
+}
+
+const getMyEvents = (allEvents) => {
+  let eventsName = [];
+  allEvents
+    .map(eventsNameMap =>
+      eventsName.push({ name: eventsNameMap.name }
+      )
+    )
+  return eventsName;
+}
 
 module.exports = router;
