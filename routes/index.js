74cac699a50d5c5662720c19f15c4ee4db68928d
JorisDenissen <joris.denissen@mail.be>
2021-03-16 21:34:31 +0100

    Implementatie error handling

--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,8 @@ const ejs = require('ejs');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-
-const url = 'https://app.ticketmaster.com/discovery/v2/events?apikey=lEQ7UsGACLAA2yaJ47Xt5hJPLK75W3Is&locale=*';
+const keyword = 'bananaSplit';
+const url = `https://app.ticketmaster.com/discovery/v2/events?apikey=lEQ7UsGACLAA2yaJ47Xt5hJPLK75W3Is&keyword=${keyword}&locale=*`;
 
 async function getapi(url) {
 
@@ -19,7 +19,6 @@ async function getapi(url) {
 
   // Storing data in form of JSON 
   var data = await response.json();
-  console.log(data);
 }
 getapi(url);
 
@@ -27,10 +26,38 @@ getapi(url);
 router.get("/", async (req, res, next) => {
   // Hier komt de fetch
   fetch(url)
-    .then((response) => response.json())
-    .then((json) =>
-      res.render("index", { result: JSON.stringify(json._embedded.events) })
-    )
+    .then((response) => {
+      if(!response.ok){
+        switch(response.status){          
+          case 400:
+            console.log('Er is een lokale error opgetreden')
+            break;
+
+          case 404:
+            console.log('De pagina kan niet gevonden worden');
+            break;
+      
+          case 500:
+            console.log('Er is een error op de server opgetreden');
+            break;
+      
+          default:
+            console.log(`Er is een ${response.status} status fout opgetreden.`);
+            break;
+        }
+      }
+      else{
+        return response.json();
+      }
+    })
+    .then((json) => {
+      if(json.page.totalElements === 0){
+        res.render("index", {result: "Geen resultaten beschikbaar"})
+      }
+      else{
+        res.render("index", { result: JSON.stringify(json._embedded.events) })
+      }
+    })
     .catch((e) => console.error(e));
 });
 
@@ -38,13 +65,14 @@ router.get('/list', async (req, res, next) => {
   const allEvents = await loadAllEventData();
 
   const toonEvents = getMyEvents(allEvents)
-  console.log(toonEvents);
   res.render('list', { allEvents: toonEvents });
 });
 
 const loadAllEventData = async () => {
+  let json;
   let response = await fetch('https://app.ticketmaster.com/discovery/v2/events?apikey=lEQ7UsGACLAA2yaJ47Xt5hJPLK75W3Is&locale=*');
-  let json = await response.json();
+  console.log(response.status);
+  json = await response.json();
   let event = json._embedded.events;
 
 
