7f832194006b0cb7a20c4ea091b4ec27de01e27c
SanneVerbist90 <60688549+SanneVerbist90@users.noreply.github.com>
2021-03-18 09:18:07 +0100

    added detail route

--- routes/index.js
+++ routes/index.js
@@ -3,30 +3,34 @@ var express = require("express");
 var router = express.Router();
 const fetch = require("node-fetch");
 const app = express();
-const ejs = require('ejs');
-//let ejs = require("ejs");
+const ejs = require("ejs");
 
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
-
-const url = 'https://app.ticketmaster.com/discovery/v2/events?apikey=lEQ7UsGACLAA2yaJ47Xt5hJPLK75W3Is&locale=*';
-
-async function getapi(url) {
-
-  // Storing response 
-  const response = await fetch(url);
-
-  // Storing data in form of JSON 
-  var data = await response.json();
-  console.log(data);
-}
-getapi(url);
-
-/* GET home page. */
+/* GET home page. All events */
 router.get("/", async (req, res, next) => {
   // Hier komt de fetch
-  fetch(url)
+  fetch(
+    `https://app.ticketmaster.com/discovery/v2/events?apikey=lEQ7UsGACLAA2yaJ47Xt5hJPLK75W3Is&locale=*`
+  )
+    .then((response) => response.json())
+    .then((json) =>
+      res.render("index", {
+        result: JSON.stringify(json._embedded.events),
+      })
+    )
+    .catch((e) => console.error(e));
+});
+
+let id = "";
+/* GET home page. Event detail */
+router.get(`/:id?`, async (req, res, next) => {
+  let id = req.params.id;
+
+  fetch(
+    `https://app.ticketmaster.com/discovery/v2/events?apikey=lEQ7UsGACLAA2yaJ47Xt5hJPLK75W3Is&id=${id}&locale=*`
+  )
     .then((response) => response.json())
     .then((json) =>
       res.render("index", { result: JSON.stringify(json._embedded.events) })
@@ -34,31 +38,20 @@ router.get("/", async (req, res, next) => {
     .catch((e) => console.error(e));
 });
 
-router.get('/list', async (req, res, next) => {
-  const allEvents = await loadAllEventData();
+// router.get("/list", async (req, res, next) => {
+//   const allEvents = await loadAllEventData();
 
-  const toonEvents = getMyEvents(allEvents)
-  console.log(toonEvents);
-  res.render('list', { allEvents: toonEvents });
-});
+//   const toonEvents = getMyEvents(allEvents);
+//   console.log(toonEvents);
+//   res.render("list", { allEvents: toonEvents });
+// });
 
-const loadAllEventData = async () => {
-  let response = await fetch('https://app.ticketmaster.com/discovery/v2/events?apikey=lEQ7UsGACLAA2yaJ47Xt5hJPLK75W3Is&locale=*');
-  let json = await response.json();
-  let event = json._embedded.events;
-
-
-  return event;
-}
-
-const getMyEvents = (allEvents) => {
-  let eventsName = [];
-  allEvents
-    .map(eventsNameMap =>
-      eventsName.push({ name: eventsNameMap.name }
-      )
-    )
-  return eventsName;
-}
+// const getMyEvents = (allEvents) => {
+//   let eventsName = [];
+//   allEvents.map((eventsNameMap) =>
+//     eventsName.push({ name: eventsNameMap.name })
+//   );
+//   return eventsName;
+// };
 
 module.exports = router;
