ded0a5d2849f53657d0a6a688a99703a18ef69f0
SanneVerbist90 <60688549+SanneVerbist90@users.noreply.github.com>
2021-05-20 15:50:36 +0200

    fix

--- routes/index.js
+++ routes/index.js
@@ -63,7 +63,14 @@ router.get("/", async (req, res, next) => {
       } else {
         console.log("test", json.page.totalElements);
 
-        res.render("index", { result: json._embedded.events });
+        res.render("index", {
+          result: json._embedded.events,
+          pageInfo: json.page,
+          currentKeyword: keyword,
+          currentCity: city,
+          currentPage: Number(page.slice(6)),
+          testNumber: 5,
+        });
       }
     })
     .catch((e) => console.error(e));
